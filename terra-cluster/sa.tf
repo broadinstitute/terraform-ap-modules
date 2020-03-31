431cf69a230dbb03b6e2df0b8538b70ee65b45ca
Greg Malkov <51164901+gmalkov@users.noreply.github.com>
2020-03-31 16:30:34 -0400

    Update sa.tf

--- terra-cluster/sa.tf
+++ terra-cluster/sa.tf
@@ -2,13 +2,12 @@
 # Service Accounts
 #
 
-# CI access
+# CI/GitHub Actions Service Account
 resource "google_service_account" "ci" {
   project      = var.google_project
   account_id   = "${local.owner}-ci-sa"
   display_name = "${local.owner}-ci-sa"
 }
-
 resource "google_project_iam_member" "ci" {
   count   = length(var.ci_sa_roles)
   project = var.google_project
