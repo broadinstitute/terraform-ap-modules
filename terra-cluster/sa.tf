ce2b27a262743b9a89bd9660427dc02129afab83
wchambers <1499870+wchamber@users.noreply.github.com>
2020-04-13 14:39:05 -0400

    Give the node pool SA the roles it needs to run a cluster. Per https://cloud.google.com/kubernetes-engine/docs/how-to/hardening-your-cluster#use_least_privilege_sa

--- terra-cluster/sa.tf
+++ terra-cluster/sa.tf
@@ -22,6 +22,24 @@ resource "google_service_account" "node_pool" {
   display_name = "${local.owner}-node-pool"
 }
 
+locals {
+  # Roles needed for the node pool service account to run a GKE cluster.
+  node_pool_gke_roles = [
+    "monitoring.viewer",
+    "monitoring.metricWriter",
+    "logging.logWriter",
+    # Google Container Registry read access.
+    "roles/storage.objectViewer"
+  ]
+}
+
+resource "google_project_iam_member" "node_pool_gke" {
+  count   = length(local.node_pool_gke_roles)
+  project = var.google_project
+  role    = element(local.node_pool_gke_roles, count.index)
+  member  = "serviceAccount:${google_service_account.node_pool.email}"
+}
+
 # Read and pull images from other_gcr_projects Google Container Registries.
 resource "google_project_iam_member" "node_pool" {
   count   = length(var.other_gcr_projects)
