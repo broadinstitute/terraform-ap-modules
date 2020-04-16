7e4a03608d13e2f9b45bdabc3447102a8041eef4
Greg Malkov <gmalkov@broadinstitute.org>
2020-04-16 11:18:54 -0400

    explicit provider

--- single-cell-portal/sa.tf
+++ single-cell-portal/sa.tf
@@ -1,24 +1,36 @@
 resource "google_service_account" "app" {
+  provider     = google.target
+
   count        = var.create_sa ? 1 : 0
+
   account_id   = local.app_sa_name
   display_name = local.app_sa_name
   project      = var.google_project
 }
 resource "google_project_iam_member" "app" {
+  provider     = google.target
+
   count   = var.create_sa ? length(var.app_sa_roles) : 0
+
   project = var.google_project
   role    = element(var.app_sa_roles, count.index)
   member  = "serviceAccount:${google_service_account.app[0].email}"
 }
 
 resource "google_service_account" "app_read" {
+  provider     = google.target
+
   count        = var.create_sa ? 1 : 0
+
   account_id   = local.app_read_sa_name
   display_name = local.app_read_sa_name
   project      = var.google_project
 }
 resource "google_project_iam_member" "app_read" {
+  provider     = google.target
+
   count   = var.create_sa ? length(var.app_read_sa_roles) : 0
+
   project = var.google_project
   role    = element(var.app_read_sa_roles, count.index)
   member  = "serviceAccount:${google_service_account.app_read[0].email}"
