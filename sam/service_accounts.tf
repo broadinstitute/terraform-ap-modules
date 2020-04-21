330797e32d383275b2b4194971e220c3d062a59b
wchambers <1499870+wchamber@users.noreply.github.com>
2020-04-21 14:29:26 -0400

    Add iam.serviceAccountUser to Sam service account. (and alphabetize)

--- sam/service_accounts.tf
+++ sam/service_accounts.tf
@@ -20,9 +20,10 @@ resource "google_service_account" "sam_admin_sdk" {
 locals {
   # Roles to give Sam on its own Google project.
   roles = [
+    "roles/iam.serviceAccountUser",
+    "roles/cloudtrace.agent",
     "roles/logging.logWriter",
     "roles/monitoring.metricWriter",
-    "roles/cloudtrace.agent"
   ]
   # Roles to give Sam on the classic Google project.
   classic_roles = [
