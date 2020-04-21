b45a151505902ef973d4c7f5f64cad8526d2e0f6
wchambers <1499870+wchamber@users.noreply.github.com>
2020-04-21 13:50:45 -0400

    fix sam module reference to be sam moudle and not sam-persistence. doh.

--- terra-env/apps.tf
+++ terra-env/apps.tf
@@ -25,7 +25,7 @@ module "identity_concentrator" {
 module "sam" {
   # TODO fix to reference.
   #source = "github.com/broadinstitute/terraform-ap-modules.git//sam?ref=sam-0.0.2"
-  source = "./../sam-persistence"
+  source = "./../sam"
   google_project                 = var.google_project
   classic_storage_google_project = var.classic_storage_google_project
   num_admin_sdk_service_accounts = 3
