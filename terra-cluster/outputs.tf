47ff107f2da0b906bc2856f36e5c68107540c530
Chelsea Hoover <chelsea@broadinstitute.org>
2020-04-30 10:05:34 -0400

    DDO-315 Make egress IPs an output of the terra-cluster module

--- terra-cluster/outputs.tf
+++ terra-cluster/outputs.tf
@@ -1,3 +1,8 @@
 output "ci_sa_id" {
   value = google_service_account.ci.account_id
 }
+
+# IPs for outgoing traffic from the cluster
+output "egress_ips" {
+  value = google_compute_address.nat-address.*.address
+}
