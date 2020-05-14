57125878d5b1e8d3b6f523808c47cb3c77a40027
Mike Flinn <mflinn@broadinstitute.org>
2020-05-14 10:04:04 -0400

    Bump monitoring module to latest

--- terra-cluster/k8s-monitoring.tf
+++ terra-cluster/k8s-monitoring.tf
@@ -1,5 +1,5 @@
 module "cluster_monitoring" {
-  source = "github.com/broadinstitute/terraform-shared.git//terraform-modules/stackdriver/k8s-cluster-monitoring?ref=k8s-cluster-monitoring-0.0.4-tf-0.12"
+  source = "github.com/broadinstitute/terraform-shared.git//terraform-modules/stackdriver/k8s-cluster-monitoring?ref=k8s-cluster-monitoring-0.0.5-tf-0.12"
   providers = {
     google.target = google.target
   }
