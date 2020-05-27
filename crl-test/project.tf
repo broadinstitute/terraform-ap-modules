7e0fdf66f69f4a8f08e29459087705a921d7ea28
wchambers <1499870+wchamber@users.noreply.github.com>
2020-05-27 10:58:46 -0400

    Replace terraform-shared/project with project resource and api services.

--- crl-test/project.tf
+++ crl-test/project.tf
@@ -1,20 +1,8 @@
-module "crl-project" {
-  source = "github.com/broadinstitute/terraform-shared.git//terraform-modules/google-project?ref=google-project-0.0.3-tf-0.12"
-
-  project_name = var.google_project
-  folder_id = var.folder_id
-  billing_account_id = var.billing_account_id
-  apis_to_enable = [
-    # APIs needed by CRL functionality.
-    "cloudtrace.googleapis.com",
-    "logging.googleapis.com",
-    "monitoring.googleapis.com",
-    # APIs exercised by CRL integration testing.
-    "storage-api.googleapis.com",
-    "storage-component.googleapis.com",
-  ]
-
-  providers = {
-    google.target = google.target
-  }
+resource "google_project" "project" {
+  provider = "google.target"
+  name                  = "${var.google_project}"
+  project_id            = "${var.google_project}"
+  billing_account       = "${var.billing_account_id}"
+  folder_id             = "${var.folder_id}"
+  auto_create_network   = false
 }
