72c2eb9b9d7481755633502a462d528e5e140536
Greg Malkov <gmalkov@broadinstitute.org>
2020-06-05 11:03:55 -0400

    make address regional

--- poc-service/ip.tf
+++ poc-service/ip.tf
@@ -1,6 +1,7 @@
-resource "google_compute_global_address" "ingress_ip" {
+resource "google_compute_address" "ingress_ip" {
   count = var.enable ? 1 : 0
 
   provider = google.target
+
   name     = "terra-${var.cluster}-${local.owner}-${local.service}-ip"
 }
