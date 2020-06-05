08ab41414284a8ad1a5f8c841ab01f9ab54c2d95
Greg Malkov <gmalkov@broadinstitute.org>
2020-06-05 11:10:35 -0400

    syntax

--- poc-service/dns.tf
+++ poc-service/dns.tf
@@ -13,5 +13,5 @@ resource "google_dns_record_set" "atlantis" {
   name         = "${local.domain_name}.${data.google_dns_managed_zone.dns_zone[0].dns_name}"
   type         = "A"
   ttl          = "300"
-  rrdatas    = [ google_compute_global_address.ingress_ip[0].address ]
+  rrdatas    = [ google_compute_address.ingress_ip[0].address ]
 }
