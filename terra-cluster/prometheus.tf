04c489d080c3e5102b41f64aa5085460f2b2625c
Mike Flinn <mflinn@broadinstitute.org>
2020-06-25 11:56:57 -0400

    Add enable flag

--- terra-cluster/prometheus.tf
+++ terra-cluster/prometheus.tf
@@ -1,6 +1,7 @@
 module "prometheus" {
   source = "github.com/broadinstitute/terraform-ap-modules.git//prometheus?ref=DDO-337-prometheus-ingress"
 
+  enable      = true
   environment = local.owner
 
   providers = {
