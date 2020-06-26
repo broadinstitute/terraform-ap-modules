5e4786ea3482f7cdb8a9c05994c83aeec3a5b290
Mike Flinn <mflinn@broadinstitute.org>
2020-06-26 11:13:39 -0400

    Use release tag

--- terra-cluster/prometheus.tf
+++ terra-cluster/prometheus.tf
@@ -1,5 +1,5 @@
 module "prometheus" {
-  source = "github.com/broadinstitute/terraform-ap-modules.git//prometheus?ref=DDO-337-prometheus-ingress"
+  source = "github.com/broadinstitute/terraform-ap-modules.git//prometheus?ref=terra-cluster-0.0.13"
 
   enable         = true
   environment    = local.owner
