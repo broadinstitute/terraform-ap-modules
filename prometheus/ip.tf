3c84de974b5f515530ca760b380ecd56798d0d40
Mike Flinn <mflinn@broadinstitute.org>
2020-07-20 10:33:40 -0400

    Explicitly declare project

--- prometheus/ip.tf
+++ prometheus/ip.tf
@@ -2,6 +2,7 @@ resource "google_compute_global_address" "ingress_ip" {
   count = var.enable ? 1 : 0
 
   provider = google.target
+  project  = var.google_project
 
   name = "terra-${local.owner}-${local.service}-ip"
 }
