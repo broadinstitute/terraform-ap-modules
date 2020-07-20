dc7abe0602e4960c7588ce74ed31bc000d70977e
Mike Flinn <mflinn@broadinstitute.org>
2020-07-20 10:23:22 -0400

    Remove unneeded project declaration

--- prometheus/ip.tf
+++ prometheus/ip.tf
@@ -2,7 +2,6 @@ resource "google_compute_global_address" "ingress_ip" {
   count = var.enable ? 1 : 0
 
   provider = google.target
-  project  = local.project
 
   name = "terra-${local.owner}-${local.service}-ip"
 }
