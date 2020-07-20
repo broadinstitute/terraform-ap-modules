be3ca4ab4400c306c4be7d4193ba8b46a7084184
Mike Flinn <mflinn@broadinstitute.org>
2020-07-20 09:47:51 -0400

    Fix to integ cluster ip being created in wrong project

--- prometheus/ip.tf
+++ prometheus/ip.tf
@@ -1,7 +1,7 @@
 resource "google_compute_global_address" "ingress_ip" {
   count = var.enable ? 1 : 0
 
-  provider = google.dns
+  provider = google.target
   project  = local.project
 
   name = "terra-${local.owner}-${local.service}-ip"
