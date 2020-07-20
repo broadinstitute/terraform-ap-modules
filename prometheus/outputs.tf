d618b3041751a4f42d3813e7e3d854f302b0e736
Mike Flinn <mflinn@broadinstitute.org>
2020-07-20 12:47:10 -0400

    REmove comments

--- prometheus/outputs.tf
+++ prometheus/outputs.tf
@@ -2,12 +2,10 @@
 # IP/DNS Outputs
 #
 output "ingress_ip" {
-  # type        = string
   description = "Prometheus ingress IP"
   value       = var.enable ? google_compute_global_address.ingress_ip[0].address : null
 }
 output "fqdn" {
-  # type        = string
   description = "Prometheus FQDN"
   value       = var.enable ? local.fqdn : null
 }
