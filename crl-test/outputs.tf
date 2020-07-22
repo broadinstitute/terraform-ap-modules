6b7ea9e019e3868a81fbd70c976f92f10eced4d5
yonghao <yyhao1@gmail.com>
2020-07-22 14:38:38 -0400

    add crl test client SA account

--- crl-test/outputs.tf
+++ crl-test/outputs.tf
@@ -14,7 +14,7 @@ output "service_account_email_user" {
   description = "User Google service account email"
 }
 
-output "service_account_email_crl_client" {
+output "service_account_email_client" {
   value       = google_service_account.crl_test_client.email
   description = "User Google service account email"
 }
