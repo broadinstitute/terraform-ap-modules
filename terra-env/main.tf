cff10394ec21f136211515b03ddd88ee9c348a62
ansingh7115 <singh.an@husky.neu.edu>
2020-07-28 14:44:26 -0400

    change wsm version

--- terra-env/main.tf
+++ terra-env/main.tf
@@ -69,7 +69,7 @@ module "sam_persistence" {
 }
 
 module "workspace_manager" {
-  source = "github.com/broadinstitute/terraform-ap-modules.git//terra-workspace-manager?ref=as367"
+  source = "github.com/broadinstitute/terraform-ap-modules.git//terra-workspace-manager?ref=terra-workspace-manager-0.3.0"
 
   enable = local.terra_apps["workspace_manager"]
 
