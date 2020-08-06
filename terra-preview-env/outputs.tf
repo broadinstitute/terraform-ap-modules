2b56cc7b75a58bb71f3c11b15f079519cd32ec2d
Greg Malkov <gmalkov@broadinstitute.org>
2020-08-06 11:39:50 -0400

    syntax

--- terra-preview-env/outputs.tf
+++ terra-preview-env/outputs.tf
@@ -2,7 +2,7 @@
 # IP/DNS Outputs
 #
 output "ingress_ips" {
-  value       = google_compute_address.ingress.*.address
+  value       = google_compute_address.ingress[*].address
   description = "Service ingress IPs"
 }
 output "fqdns" {
