24ef80d97cef4ff60f17496197ede18c52ddd865
yonghao <yyhao1@gmail.com>
2020-08-10 18:55:52 -0400

    folder_id

--- crl-janitor/sa.tf
+++ crl-janitor/sa.tf
@@ -19,7 +19,7 @@ resource "google_project_iam_member" "app_roles" {
 # Grant Janitor App Service Account editor permission in folder level permission to cleanup resources.
 resource "google_folder_iam_member" "app_folder_roles" {
   // Skip if google_folder variable is not present.
-  count = var.enable && (var.google_folder != "") ? 1 : 0
+  count = var.enable && (var.google_folder_id != "") ? 1 : 0
 
   provider = google.target
   folder  = var.google_folder_id
