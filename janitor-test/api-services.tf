441dc830e944af355076623add454e227ba7fe3a
yonghao <yyhao1@gmail.com>
2020-08-12 19:06:31 -0400

    provision a project for janitor

---
+++ janitor-test/api-services.tf
@@ -0,0 +1,14 @@
+module "enable-services" {
+  source      = "github.com/broadinstitute/terraform-shared.git//terraform-modules/api-services?ref=services-0.3.0-tf-0.12"
+  enable_flag = "1"
+  providers = {
+    google.target = google.target
+  }
+  project = google_project.project.name
+  services = [
+    # APIs exercised by Janitor integration testing.
+    "storage-api.googleapis.com",
+    "storage-component.googleapis.com",
+    "bigquery.googleapis.com",
+  ]
+}
