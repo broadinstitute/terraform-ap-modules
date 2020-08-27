0908b0605f31aa26ed99ecac39177453e5c40315
mflinn-broad <mflinn@broadinstitute.org>
2020-08-27 17:02:32 -0400

    sigh

--- terra-env/main.tf
+++ terra-env/main.tf
@@ -135,7 +135,7 @@ module "datarepo" {
 }
 
 module "ontology" {
-  source = "github.com/broadinstitute/terraform-ap-modules.git//ontology?ref=ontology-0.1.2"
+  source = "github.com/broadinstitute/terraform-ap-modules.git//ontology?ref=DDO-634-support-ontology-k8s"
 
   enable = local.terra_apps["ontology"]
 
