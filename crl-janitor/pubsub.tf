2925b197365803494106306fd5432c069ea1e825
yonghao <yyhao1@gmail.com>
2020-08-27 15:31:40 -0400

    grant project SA pubsub permisson for DLQ

--- crl-janitor/pubsub.tf
+++ crl-janitor/pubsub.tf
@@ -32,6 +32,21 @@ resource "google_pubsub_subscription" "crl-janitor-pubsub-subscription" {
   }
 }
 
+resource "google_pubsub_subscription" "crl-janitor-pubsub-dead-letter-subscription" {
+  count = var.enable ? 1 : 0
+
+  project = var.google_project
+  name = "${local.service}-${local.owner}-dead-letter-pubsub-sub"
+  topic = google_pubsub_topic.crl-janitor-pubsub-dead-letter-topic[0].name
+
+  ack_deadline_seconds = 20
+
+  expiration_policy {
+    // No expiration
+    ttl = ""
+  }
+}
+
 # Janitor clients can publish to the topic
 resource "google_pubsub_topic_iam_member" "crl_janitor_client_can_publish" {
   count = var.enable ? 1 : 0
@@ -49,4 +64,25 @@ resource "google_pubsub_subscription_iam_member" "crl_janitor_client_can_manage"
   subscription = google_pubsub_subscription.crl-janitor-pubsub-subscription[0].name
   role = "roles/editor"
   member   = "serviceAccount:${google_service_account.app[0].email}"
+}
+
+# Project SA can publish to the dead letter queue topic
+resource "google_pubsub_topic_iam_member" "crl_janitor_client_can_publish_dead_letter" {
+  count = var.enable ? 1 : 0
+
+  project = var.google_project
+  topic = google_pubsub_topic.crl-janitor-pubsub-dead-letter-topic[0].name
+  role = "roles/pubsub.publisher"
+
+  member   = data.google_service_account.project_pubsub_sa.email
+}
+
+# Project SA can subscribe to the dead letter queue topic
+resource "google_pubsub_subscription_iam_member" "crl_janitor_client_can_subscribe_dead_letter" {
+  count = var.enable ? 1 : 0
+
+  project = var.google_project
+  subscription = google_pubsub_subscription.crl-janitor-pubsub-dead-letter-subscription[0].name
+  role = "roles/pubsub.subscriber"
+  member   = data.google_service_account.project_pubsub_sa.email
 }
