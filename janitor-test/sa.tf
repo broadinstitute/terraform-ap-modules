b2c3be3a21ca966601a38c2db5ded4525bd787da
yonghao <yyhao1@gmail.com>
2020-09-11 11:45:23 -0400

    folder admin

---
+++ janitor-test/sa.tf
@@ -0,0 +1,31 @@
+locals {
+  folder_roles = [
+    "roles/resourcemanager.folderAdmin",
+    "roles/resourcemanager.projectCreator",
+    "roles/resourcemanager.projectDeleter",
+  ]
+}
+
+# The main service account for the Janitor service app.
+resource "google_service_account" "app" {
+  provider     = google.target
+  project      = var.google_project
+  account_id   = "janitor-test-resource-access"
+  display_name = "janitor-test-resource-access"
+}
+
+resource "google_project_iam_member" "app_roles" {
+  provider = google.target
+  project  = var.google_project
+  role     = local.app_sa_roles[count.index]
+  member   = "serviceAccount:${google_service_account.app[0].email}"
+}
+
+# Grant Janitor App Service Account editor permission in folder level permission to cleanup resources.
+resource "google_folder_iam_member" "app_folder_roles" {
+  provider = google.target
+  folder  = var.folder_id
+  role     = "roles/owner"
+  member   = "serviceAccount:${google_service_account.app[0].email}"
+}
+
