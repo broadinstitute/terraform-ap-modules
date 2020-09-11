e56e54ba463db5305fa21012c5c201b7b3d1f42e
yonghao <yyhao1@gmail.com>
2020-09-11 13:47:39 -0400

    rename SA name

--- janitor-test/sa.tf
+++ janitor-test/sa.tf
@@ -7,7 +7,7 @@ locals {
 }
 
 # The service account used to access cloud resources in test.
-resource "google_service_account" "app" {
+resource "google_service_account" "janitor_test" {
   provider     = google.target
   project      = var.google_project
   account_id   = "janitor-test-resource-access"
@@ -15,10 +15,10 @@ resource "google_service_account" "app" {
 }
 
 # Grant this SA Account permission to create/modify resources in test.
-resource "google_folder_iam_member" "app_folder_roles" {
+resource "google_folder_iam_member" "janitor_test_folder_roles" {
   count    = length(local.folder_roles)
   provider = google.target
   folder   = var.folder_id
   role     = local.folder_roles[count.index]
-  member   = "serviceAccount:${google_service_account.app.email}"
+  member   = "serviceAccount:${google_service_account.janitor_test.email}"
 }
