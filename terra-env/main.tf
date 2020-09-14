71ef60d9493fcee15985adfef9b8da9abe1329cb
yonghao <yyhao1@gmail.com>
2020-09-14 00:17:16 -0400

    bump version

--- terra-env/main.tf
+++ terra-env/main.tf
@@ -93,7 +93,8 @@ module "workspace_manager" {
 }
 
 module "crl_janitor" {
-  source = "github.com/broadinstitute/terraform-ap-modules.git//crl-janitor?ref=crl-janitor-0.2.5"
+  // source = "github.com/broadinstitute/terraform-ap-modules.git//crl-janitor?ref=crl-janitor-0.2.6"
+  source = "github.com/broadinstitute/terraform-ap-modules.git//crl-janitor?ref=crl-janitor-yyu-PF-24-3"
 
   enable = local.terra_apps["crl_janitor"]
 
