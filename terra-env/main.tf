9688e1172f79ee039ac0336c3157e2550577b2d6
Greg Malkov <gmalkov@broadinstitute.org>
2020-09-18 15:24:20 -0400

    lock version

--- terra-env/main.tf
+++ terra-env/main.tf
@@ -69,7 +69,7 @@ module "sam_persistence" {
 }
 
 module "workspace_manager" {
-  source = "github.com/broadinstitute/terraform-ap-modules.git//terra-workspace-manager?ref=gm-preview-consolidate"
+  source = "github.com/broadinstitute/terraform-ap-modules.git//terra-workspace-manager?ref=terra-workspace-manager-0.3.2"
 
   enable = local.terra_apps["workspace_manager"]
 
