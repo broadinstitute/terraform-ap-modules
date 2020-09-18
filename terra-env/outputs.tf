05289c9fb20b2fdebeaa40007c3f5559ba18dbea
Greg Malkov <gmalkov@broadinstitute.org>
2020-09-18 15:00:38 -0400

    syntax

--- terra-env/outputs.tf
+++ terra-env/outputs.tf
@@ -206,17 +206,17 @@ output "ontology_fqdn" {
 # Preview Environment Outputs
 #
 output "versions" {
-  value       = var.preview ? var.versions : null
+  value       = contains(["preview"], var.env_type) ? var.versions : null
   description = "Base64 encoded JSON string of version overrides"
 }
 output "ingress_ips" {
-  value       = var.preview ? {
+  value       = contains(["preview"], var.env_type) ? {
     workspacemanager = local.terra_apps["workspace_manager"] ? module.workspace_manager.ingress_ip : null
   } : null
   description = "Service ingress IPs"
 }
 output "fqdns" {
-  value       = var.preview ? {
+  value       = contains(["preview"], var.env_type) ? {
     workspacemanager = local.terra_apps["workspace_manager"] ? module.workspace_manager.fqdn : null
   } : null
   description = "Service fully qualified domain names"
