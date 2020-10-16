9f324c3b12f73a2cfb8f2a201c26b2ba0f8599a9
yonghao <yyhao1@gmail.com>
2020-10-16 15:41:52 -0400

    correct enabled

--- rbs/api-services.tf
+++ rbs/api-services.tf
@@ -1,6 +1,6 @@
 module "enable-services" {
   source      = "github.com/broadinstitute/terraform-shared.git//terraform-modules/api-services?ref=services-0.3.0-tf-0.12"
-  enable_flag = "1"
+  enable_flag = var.enable ? "1" : "0"
   providers = {
     google.target = google.target
   }
