6bb38f6ba72032dd88f2e7677b2c788a6250c3d6
mflinn-broad <mflinn@broadinstitute.org>
2020-10-23 10:04:42 -0400

    run terraform-docs

---
+++ rawls/main.tf
@@ -0,0 +1,9 @@
+/**
+ * # Terra rawls module
+ * 
+ * For more information, check out the [MC-Terra deployment doc](https://docs.dsp-devops.broadinstitute.org/mc-terra/mcterra-deployment)
+ * and our [Terraform best practices](https://docs.dsp-devops.broadinstitute.org/best-practices-guides/terraform).
+ *
+ * This documentation is generated with [terraform-docs](https://github.com/segmentio/terraform-docs)
+ * `terraform-docs markdown --no-sort . > README.md`
+ */
