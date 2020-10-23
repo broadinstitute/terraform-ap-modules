7c613557d966c07e08ffeae35ca85cac6199593c
mflinn-broad <mflinn@broadinstitute.org>
2020-10-23 10:01:00 -0400

    Add support for rawls to terra-env

--- terra-env/main.tf
+++ terra-env/main.tf
@@ -195,7 +195,24 @@ module "rbs" {
 module "consent" {
   source = "github.com/broadinstitute/terraform-ap-modules.git//consent?ref=consent-0.0.2"
 
-  enable = local.terra_apps["consent"]
+  enable = local.terra_apps["rawls"]
+
+  google_project = var.google_project
+  cluster        = var.cluster
+  cluster_short  = var.cluster_short
+
+  dns_zone_name  = var.dns_zone_name
+  subdomain_name = var.subdomain_name
+  use_subdomain  = var.use_subdomain
+
+  providers = {
+    google.target = google.target
+    google.dns    = google.dns
+  }
+}
+
+module "rawls" {
+  source = "github.com/broadinstitute/terraform-ap-modules.git//consent?ref=DDO-824-expose-rawls-k8s"
 
   google_project = var.google_project
   cluster        = var.cluster
