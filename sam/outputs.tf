d2f4fc6ad6fd9238c2c92853f001faf0d6fa49fc
Greg Malkov <gmalkov@broadinstitute.org>
2020-10-26 15:41:36 -0400

    update outputs

--- sam/outputs.tf
+++ sam/outputs.tf
@@ -2,15 +2,15 @@
 # Service Account Outputs
 #
 output "app_sa_id" {
-  value       = var.enable && contains(["default", "preview_shared"], var.env_type) ? google_service_account.sam[0].account_id : null
+  value       = var.enable && contains(["preview_shared"], var.env_type) ? google_service_account.sam[0].account_id : null
   description = "Sam app Google service account ID"
 }
 output "admin_sdk_sa_ids" {
-  value       = var.enable && contains(["default", "preview_shared"], var.env_type) ? google_service_account.sam_admin_sdk.*.account_id : null
+  value       = var.enable && contains(["preview_shared"], var.env_type) ? google_service_account.sam_admin_sdk.*.account_id : null
   description = "Sam admin SDK Google service account IDs"
 }
 output "firestore_sa_email" {
-  value       = var.enable && contains(["default", "preview_shared"], var.env_type) ? google_service_account.sam-firestore[0].email : null
+  value       = var.enable && contains(["preview_shared"], var.env_type) ? google_service_account.sam-firestore[0].email : null
   description = "Sam Firestore Google service account email"
 }
 
@@ -34,6 +34,6 @@ output "fqdn" {
 # Firestore Outputs
 #
 output "firestore_project_name" {
-  value = var.enable && contains(["default", "preview_shared"], var.env_type) ? google_project.sam-firestore[0].name : null
+  value = var.enable && contains(["preview_shared"], var.env_type) ? google_project.sam-firestore[0].name : null
   description = "Sam Firestore project name"
 }
