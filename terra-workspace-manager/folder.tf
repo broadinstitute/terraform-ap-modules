21e5d31bd24c8a881de8e5fe693466e2e44e87db
wchambers <1499870+wchamber@users.noreply.github.com>
2020-10-27 14:15:12 -0400

    Try rolling back to using null to see if it still works

--- terra-workspace-manager/folder.tf
+++ terra-workspace-manager/folder.tf
@@ -2,7 +2,7 @@
 # We create a separate folder to scope the WM SA broad folder permissions to a single purpose folder.
 # TODO(PF-156): Once WM uses RBS, we no longer need permissions to create projects, or this folder.
 resource "google_folder" "workspace_project_folder" {
-  count        = var.enable && contains(["default", "preview_shared"], var.env_type) && var.workspace_project_folder_id != "" ? 1 : 0
+  count        = var.enable && contains(["default", "preview_shared"], var.env_type) && var.workspace_project_folder_id != null ? 1 : 0
   display_name = "${local.service}-${local.owner} workspace project folder"
   parent       = "folders/${var.workspace_project_folder_id}"
   provider     = google.target
