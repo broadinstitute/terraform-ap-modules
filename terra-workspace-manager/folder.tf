4b5f4edaecd180dceb8e11365a5670d1d3fd76b3
wchambers <1499870+wchamber@users.noreply.github.com>
2020-10-27 14:47:34 -0400

    shorten folder display name to try to meet 30 char limit

--- terra-workspace-manager/folder.tf
+++ terra-workspace-manager/folder.tf
@@ -3,7 +3,7 @@
 # TODO(PF-156): Once WM uses RBS, we no longer need permissions to create projects, or this folder.
 resource "google_folder" "workspace_project_folder" {
   count        = var.enable && contains(["default", "preview_shared"], var.env_type) && var.workspace_project_folder_id != "" ? 1 : 0
-  display_name = "${local.service}-${local.owner} workspace project folder"
+  display_name = "${local.service}-${local.owner} projects"
   parent       = "folders/${var.workspace_project_folder_id}"
   provider     = google.target
 }
