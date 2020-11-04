e79faf96e8e88c3b194f2519a94a3437d3b3f4f3
Chelsea Hoover <chelsea@broadinstitute.org>
2020-11-04 09:26:23 -0500

    Pin to new wsm module tag

--- terra-env/main.tf
+++ terra-env/main.tf
@@ -72,7 +72,7 @@ module "sam" {
 }
 
 module "workspace_manager" {
-  source = "github.com/broadinstitute/terraform-ap-modules.git//terra-workspace-manager?ref=da_AS458_upgradePostgres"
+  source = "github.com/broadinstitute/terraform-ap-modules.git//terra-workspace-manager?ref=terra-workspace-manager-0.4.3"
 
   enable = local.terra_apps["workspace_manager"]
 
