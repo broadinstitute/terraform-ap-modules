635336f4067f2865c23c756c38f8205b4c7deada
mflinn-broad <mflinn@broadinstitute.org>
2020-11-13 12:01:03 -0500

    use release tag

--- terra-env/main.tf
+++ terra-env/main.tf
@@ -193,7 +193,7 @@ module "rbs" {
 }
 
 module "consent" {
-  source = "github.com/broadinstitute/terraform-ap-modules.git//consent?ref=DDO-849-consent-gke-ingress"
+  source = "github.com/broadinstitute/terraform-ap-modules.git//consent?ref=consent-0.2.0"
 
   enable = local.terra_apps["consent"]
 
