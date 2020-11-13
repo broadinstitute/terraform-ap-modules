0d76e12d4e7c602741dff8cd31d161177542686a
mflinn-broad <60187023+mflinn-broad@users.noreply.github.com>
2020-11-13 13:22:32 -0500

    DDO-849 consent-gke-ingress (#104)
    * update consent module to support global static ip
    
    * fix typo
    
    * use release tag

--- terra-env/main.tf
+++ terra-env/main.tf
@@ -193,7 +193,7 @@ module "rbs" {
 }
 
 module "consent" {
-  source = "github.com/broadinstitute/terraform-ap-modules.git//consent?ref=consent-0.0.2"
+  source = "github.com/broadinstitute/terraform-ap-modules.git//consent?ref=consent-0.2.0"
 
   enable = local.terra_apps["consent"]
 
