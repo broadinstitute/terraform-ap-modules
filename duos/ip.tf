79e2974f8cda41bc263395282c931013baf254c2
mflinn-broad <mflinn@broadinstitute.org>
2020-12-15 14:43:01 -0500

    and set project again

--- duos/ip.tf
+++ duos/ip.tf
@@ -2,6 +2,8 @@ resource "google_compute_global_address" "ingress_ip" {
   count = var.enable ? 1 : 0
 
   provider = google.target
+  project  = var.google_project
 
   name = "terra-${var.cluster}-${local.owner}-${local.service}-ip"
 }
+
