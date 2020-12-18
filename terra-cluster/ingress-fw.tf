cf10b4784a4abcd8345d36de7ab6627b2cef9190
Chelsea Hoover <chelsea@broadinstitute.org>
2020-12-18 11:38:30 -0500

    DDO-853 Private ingress fix

--- terra-cluster/ingress-fw.tf
+++ terra-cluster/ingress-fw.tf
@@ -1,13 +1,19 @@
 #
 # Create a CloudArmor policy for private Terra ingresses.
 #
+locals {
+  # Make sure Cloud NAT egress addresses are whitelisted in the Ingress firewall,
+  # so that traffic originating from within the cluster can pass through the Ingress
+  cluster_egress_cidrs  = google_compute_address.nat-address.*.address
+  private_ingress_cidrs = concat(var.private_ingress_cidrs, local.cluster_egress_cidrs)
+}
 resource "google_compute_security_policy" "terra-private-ingress-policy" {
   name = "${module.k8s-master.name}-private-ingress-policy"
 
   # Chunk all cidrs into groups of 5 (max allowed in a rule),
   # add add a whitelist rule for each chunk
   dynamic "rule" {
-    for_each = chunklist(var.private_ingress_cidrs, 5)
+    for_each = chunklist(local.private_ingress_cidrs, 5)
 
     content {
       action   = "allow"
@@ -21,6 +27,14 @@ resource "google_compute_security_policy" "terra-private-ingress-policy" {
         }
       }
     }
+  }
+
+  rule {
+    action      = "allow"
+    priority    = size(var.private_ingress_cidrs)
+    description = "Allow traffic originating from within the cluster"
+
+    match {}
   }
 
   rule {
