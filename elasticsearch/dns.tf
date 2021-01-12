3d203304d5a654d05b5216446924838fd9fd1918
mflinn-broad <mflinn@broadinstitute.org>
2021-01-12 13:45:04 -0500

    more syntax tweaks

--- elasticsearch/dns.tf
+++ elasticsearch/dns.tf
@@ -11,9 +11,9 @@ locals {
 resource "google_dns_record_set" "ingress" {
   project      = var.google_project
   provider     = google.dns
-  managed_zone = data.google_dns_managed_zone.dns_zone[0].name
+  managed_zone = data.google_dns_managed_zone.dns_zone.name
   name         = local.fqdn
   type         = "A"
   ttl          = "300"
-  rrdatas      = [google_compute_global_address.ingress_ip[0].address]
+  rrdatas      = [google_compute_global_address.ingress_ip.address]
 }
