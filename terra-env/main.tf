87366ad7087c22237cc494abfc840f1e3a72b13e
Tova Nadler <tnadler@broadinstitute.org>
2021-01-14 10:00:36 -0500

    Use tagged versions of buffer and workspace manager

--- terra-env/main.tf
+++ terra-env/main.tf
@@ -72,7 +72,7 @@ module "sam" {
 }
 
 module "workspace_manager" {
-  source = "github.com/broadinstitute/terraform-ap-modules.git//terra-workspace-manager?ref=tn-PF-324-WSM-access-RBS-folders"
+  source = "github.com/broadinstitute/terraform-ap-modules.git//terra-workspace-manager?ref=terra-workspace-manager-0.4.5"
 
   enable = local.terra_apps["workspace_manager"]
 
@@ -170,7 +170,7 @@ module "ontology" {
 }
 
 module "buffer" {
-  source = "github.com/broadinstitute/terraform-ap-modules.git//buffer?ref=tn-PF-323-add-folder-creation-bool"
+  source = "github.com/broadinstitute/terraform-ap-modules.git//buffer?ref=buffer-0.1.1"
 
   enable = local.terra_apps["buffer"]
 
