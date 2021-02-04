a0fe8a327d4486526ce3a73cf826dccc4f5c6dbe
Chelsea Hoover <chelsea@broadinstitute.org>
2021-02-04 13:31:26 -0500

    fix

--- mongodb/backup.tf
+++ mongodb/backup.tf
@@ -39,5 +39,5 @@ resource "google_storage_bucket_iam_binding" "binding" {
   bucket   = google_storage_bucket.backup-bucket.name
   provider = google.target
   role     = "roles/storage.objectCreator"
-  members  = ["user:${google_service_account.backup-sa.email}"]
+  members  = ["serviceAccount:${google_service_account.backup-sa.email}"]
 }
