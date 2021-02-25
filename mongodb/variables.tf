21c69eb21af338e129276e9ab946e3517a0b23b9
Chelsea Hoover <60902147+choover-broad@users.noreply.github.com>
2021-02-25 10:31:37 -0500

    DDO-1088 static IPs for MongoDB replica ingress (#156)

--- mongodb/variables.tf
+++ mongodb/variables.tf
@@ -6,6 +6,21 @@ variable "owner" {
   description = "Environment or developer. Defaults to TF workspace name if left blank."
   default     = ""
 }
+variable "cluster" {
+  type        = string
+  description = "Terra GKE cluster suffix, whatever is after terra-"
+}
 locals {
   owner = var.owner == "" ? terraform.workspace : var.owner
+}
+
+variable "expose" {
+  type        = bool
+  description = "If true, create ingress IPs for MongoDB replicas"
+  default     = false
+}
+variable "replica_count" {
+  type        = number
+  description = "Number of replicas to create ingress IPs for"
+  default     = 3
 }
