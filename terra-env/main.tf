123e7b3a3e5c3bb908eb1fa954359fcb984f0856
Chelsea Hoover <chelsea@broadinstitute.org>
2021-03-10 13:52:57 -0500

    bump

--- terra-env/main.tf
+++ terra-env/main.tf
@@ -72,7 +72,7 @@ module "sam" {
 }
 
 module "workspace_manager" {
-  source = "github.com/broadinstitute/terraform-ap-modules.git//terra-workspace-manager?ref=ch-DDO-1107"
+  source = "github.com/broadinstitute/terraform-ap-modules.git//terra-workspace-manager?ref=terra-workspace-manager-0.6.4"
 
   enable = local.terra_apps["workspace_manager"]
 
