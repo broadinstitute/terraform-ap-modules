31ab65b0361cae0821ed782f13552231ca849ba6
Chelsea Hoover <chelsea@broadinstitute.org>
2021-03-19 09:40:59 -0400

    DDO-1105 new module to provision ingress IP + DNS for Job Manager

---
+++ jobmanager/outputs.tf
@@ -0,0 +1,11 @@
+#
+# IP/DNS Outputs
+#
+output "ingress_ip" {
+  value       = var.enable ? google_compute_global_address.ingress_ip[0].address : null
+  description = "Job Manager ingress IP"
+}
+output "fqdn" {
+  value       = var.enable ? local.fqdn : null
+  description = "Job Manager fully qualified domain name"
+}
