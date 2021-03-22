5dc15aceb54587efa97774013748fcf7b6a8b0dc
wchambers <1499870+wchamber@users.noreply.github.com>
2021-03-22 15:09:12 -0400

    Auto-create a network for crl test.

--- crl-test/project.tf
+++ crl-test/project.tf
@@ -3,5 +3,5 @@ resource "google_project" "project" {
   project_id          = var.google_project
   billing_account     = var.billing_account_id
   folder_id           = var.folder_id
-  auto_create_network = false
+  auto_create_network = true
 }
