5bb1cd95a1c8a6475e8c88e309792531d7bcc620
mflinn-broad <mflinn@broadinstitute.org>
2021-03-26 09:42:19 -0400

    update cluster module

--- terra-cluster/k8s-monitoring.tf
+++ terra-cluster/k8s-monitoring.tf
@@ -1,5 +1,5 @@
 module "cluster_monitoring" {
-  source = "github.com/broadinstitute/terraform-shared.git//terraform-modules/stackdriver/k8s-cluster-monitoring?ref=k8s-cluster-monitoring-0.0.7-tf-0.12"
+  source = "github.com/broadinstitute/terraform-shared.git//terraform-modules/stackdriver/k8s-cluster-monitoring?ref=stackdriver-0.2.3"
   providers = {
     google.target = google.target
   }
