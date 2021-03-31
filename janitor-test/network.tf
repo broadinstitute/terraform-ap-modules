68669c552a98dd413706fb63b590c55e06d20981
wchambers <1499870+wchamber@users.noreply.github.com>
2021-03-31 09:49:40 -0400

    add a vpc_network for AI notebooks.

---
+++ janitor-test/network.tf
@@ -0,0 +1,6 @@
+# Create a VPC network for testing AI Notebooks, which require a network.
+resource "google_compute_network" "vpc_network" {
+  name = "network"
+  auto_create_subnetworks = true
+  project = google_project.project
+}
