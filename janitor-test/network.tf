63580af5b582ba16dc965bb25b9ed64b5b0c919a
wchambers <1499870+wchamber@users.noreply.github.com>
2021-03-31 10:00:32 -0400

    try 'default' network name

--- janitor-test/network.tf
+++ janitor-test/network.tf
@@ -1,6 +1,6 @@
 # Create a VPC network for testing AI Notebooks, which require a network.
 resource "google_compute_network" "vpc_network" {
-  name = "network"
+  name = "default"
   auto_create_subnetworks = true
   project = google_project.project.project_id
 }
