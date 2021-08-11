2f36a6d63b20e3198bbe45faee9e854fbf813770
aweng98 <bweng@broadinstitute.org>
2021-08-11 18:43:14 -0400

    remove unused variables

--- testrunner/variables.tf
+++ testrunner/variables.tf
@@ -1,14 +1,3 @@
-variable "dependencies" {
-  # See: https://github.com/hashicorp/terraform/issues/21418#issuecomment-495818852
-  type        = any
-  default     = []
-  description = "Work-around for Terraform 0.12's lack of support for 'depends_on' in custom modules."
-}
-variable "enable" {
-  type        = bool
-  description = "Enable flag for this module. If set to false, no resources will be created."
-  default     = true
-}
 variable "google_project" {
   type        = string
   description = "The google project in which to create resources"
@@ -16,15 +5,6 @@ variable "google_project" {
 variable "sourcewriter_sa_email" {
   type        = string
   description = "The email of the service account that will write files to the source bucket"
-}
-variable "cluster" {
-  type        = string
-  description = "Terra GKE cluster suffix, whatever is after terra-"
-}
-variable "cluster_short" {
-  type        = string
-  description = "Optional short cluster name"
-  default     = ""
 }
 variable "k8s_namespace" {
   type        = string
