5517c5cbee01bb2aaa8f42507d204039d70566c4
ansingh7115 <ansingh@broadinstitute.org>
2021-08-12 00:46:42 -0400

    enable maintenance_window

--- terra-workspace-manager/cloudsql.tf
+++ terra-workspace-manager/cloudsql.tf
@@ -26,9 +26,7 @@ module "cloudsql-pg13" {
     record_client_address   = true
   }
 
-  cloudsql_maintenance_window_day = 6
-  cloudsql_maintenance_window_hour = 2
-  cloudsql_maintenance_window_update_track = "stable"
+  cloudsql_maintenance_window_enable = 1
 
   app_dbs = {
     "${local.service}" = {
