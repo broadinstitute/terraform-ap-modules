9623bfabc169e12a9dd23200d52ba821e8042ecd
mflinn-broad <mflinn@broadinstitute.org>
2021-08-30 16:53:06 -0400

    attempt to use managed cert

---
+++ duos/ssl.tf
@@ -0,0 +1,17 @@
+resource "google_compute_managed_ssl_certificate" "duos_cert" {
+  count    = var.enable ? 1 : 0
+  project  = var.google_project
+  provider = google.target
+  name     = "terra-${var.cluster}-${local.owner}-${local.service}-managed-cert"
+
+  managed {
+    domains = local.managed_domains
+  }
+}
+
+locals {
+  base_managed_domains = [
+    local.fqdn
+  ]
+  managed_domains = terraform.workspace == "prod" ? concat(base_managed_domains, "duos.broadinstitute.org") : base_managed_domains
+}
