5aa22dd2f0886b69e4621fb56dde749473b6a0d1
Isaac Zarsky <zarsky@broadinstitute.org>
2021-09-13 16:48:30 -0400

    fix instructions

--- externalcreds/main.tf
+++ externalcreds/main.tf
@@ -1,12 +1,12 @@
 /**
  * # Terra External Credentials Manager module
- * 
- * This module creates the service account and CloudSQL databases necessary for 
+ *
+ * This module creates the service account and CloudSQL databases necessary for
  * running the [External Credentials Manager](http://github.com/databiosphere/terra-external-credentials-manager).
  *
  * For more information, check out the [MC-Terra deployment doc](https://docs.dsp-devops.broadinstitute.org/mc-terra/mcterra-deployment)
  * and our [Terraform best practices](https://docs.dsp-devops.broadinstitute.org/best-practices-guides/terraform).
  *
  * This documentation is generated with [terraform-docs](https://github.com/segmentio/terraform-docs)
- * `terraform-docs markdown --no-sort . > README.md`
+ * `terraform-docs markdown . > README.md`
  */
