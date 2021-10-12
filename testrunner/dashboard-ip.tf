a0f836f6f8fa8c91dc0463ea1cbc233d6bd00d76
Ivan <ichang@broadinstitute.org>
2021-10-12 08:30:12 -0400

    Updated comments.

--- testrunner/dashboard-ip.tf
+++ testrunner/dashboard-ip.tf
@@ -4,4 +4,5 @@ resource "google_compute_global_address" "ingress_ip" {
   provider = google.target
 
   name = "terra-${var.cluster}-${local.owner}-${local.dashboardservice}-ip"
+  description = "The ingress IP of TestRunner Dashboard"
 }
--- testrunner/dashboard.tf
+++ testrunner/dashboard.tf
@@ -1,21 +1,26 @@
 ## Workload Identity Resources for TestRunner Dashboard
 
-# Main service account for TesRunner dashboard operations
-# IAM service accounts from other projects can grant roles/iam.serviceAccountUser role
-# to this service account so that the TestRunner dashboard can access resources in other projects.
-resource "google_service_account" "testrunner_dashbaord_sa" {
+# Workload Identity service account for TesRunner Dashboard operations.
+# This service account allow pool identities to access resources in Google Cloud projects,
+# e.g. by granting roles/iam.serviceAccountUser role to this service account.
+resource "google_service_account" "testrunner_dashboard_workload_sa" {
   count        = var.enable_dashboard ? 1 : 0
   provider     = google.target
   project      = var.google_project
-  account_id   = "${local.dashboardservice}-${local.owner}"
-  display_name = "${local.dashboardservice}-${local.owner}"
-  description  = "The IAM Service Account that can act on behalf of the TestRunner Dashboard Kubernetes Service Account"
+  account_id   = "${local.dashboardservice}-workload-${local.owner}"
+  display_name = "${local.dashboardservice}-workload-${local.owner}"
+  description  = "The Workload Identity IAM Service Account that the TestRunner Dashboard Kubernetes Service Account can impersonate"
 }
 
 # Allow the Kubernetes service account to impersonate the Google service account
+# by creating an IAM policy binding between the two. This binding allows the
+# Kubernetes Service account to act as the Google service account.
+#
+# Non-authoritative. Updates the IAM policy to grant roles/iam.workloadIdentityUser role to the workload identity pool associated with the Kubernetes service account.
+# Other members for the role for the service account are preserved.
 resource "google_service_account_iam_member" "testrunner_dashboard_workload_identity_iam_role" {
   count              = var.enable_dashboard ? 1 : 0
-  service_account_id = google_service_account.testrunner_dashbaord_sa[0].name
+  service_account_id = google_service_account.testrunner_dashboard_workload_sa[0].name
   role               = "roles/iam.workloadIdentityUser"
-  member             = "serviceAccount:${var.google_project}.svc.id.goog[${var.dashboard_namespace}/${var.dashboard_namespace}-ksa]"
+  member             = "serviceAccount:${var.google_project}.svc.id.goog[${var.dashboard_namespace}/${local.dashboardservice}-service-ksa]"
 }
