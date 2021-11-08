943927907521b28ea725e5f0f212e5a59edf5a51
Jackie Roberti <jacqueline.roberti93@gmail.com>
2021-11-08 13:24:55 -0500

    update reference to test changes

--- terra-env/main.tf
+++ terra-env/main.tf
@@ -71,7 +71,7 @@ module "sam" {
   firestore_folder_id          = var.sam_firestore_folder_id
 }
 module "workspace_manager" {
-  source = "github.com/broadinstitute/terraform-ap-modules.git//terra-workspace-manager?ref=terra-workspace-manager-0.7.10"
+  source = "github.com/broadinstitute/terraform-ap-modules.git//terra-workspace-manager?ref=jr-wsm-db-flags"
 
   enable = local.terra_apps["workspace_manager"]
 
