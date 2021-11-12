381ebc0d89f25ffbed0c452324478e693e919a4d
mflinn-broad <60187023+mflinn-broad@users.noreply.github.com>
2021-11-12 10:52:41 -0500

    add ip/dns for thanos sidecar (#210)

--- prometheus/ip.tf
+++ prometheus/ip.tf
@@ -6,3 +6,12 @@ resource "google_compute_global_address" "ingress_ip" {
 
   name = "terra-${local.owner}-${local.service}-ip"
 }
+
+resource "google_compute_address" "thanos_sidecar_ip" {
+  count = var.enable_thanos ? 1 : 0
+
+  provider = google.target
+  project  = var.google_project
+
+  name = "terra-${local.owner}-thanos-ip"
+}
--- prometheus/outputs.tf
+++ prometheus/outputs.tf
@@ -9,3 +9,13 @@ output "fqdn" {
   description = "Prometheus FQDN"
   value       = var.enable ? local.fqdn : null
 }
+
+output "thanos_ip" {
+  description = "ip address to access thanos sidecar"
+  value       = var.enable_thanos ? google_compute_address.thanos_sidecar_ip[0].address : null
+}
+
+output "thanos_fqdn" {
+  description = "FQDN to access thanos sidecar"
+  value       = var.enable_thanos ? local.fqdn : null
+}
