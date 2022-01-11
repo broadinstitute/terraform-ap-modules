9d348d6cedd506f6f41be949e7dfeae5b79c277b
Jack Warren <jack@jackwarren.info>
2022-01-11 13:26:32 -0500

    bump

--- poc-service/cloudsql.tf
+++ poc-service/cloudsql.tf
@@ -1,5 +1,5 @@
 module "cloudsql" {
-  source = "github.com/broadinstitute/terraform-shared.git//terraform-modules/cloudsql-postgres?ref=cloudsql-postgres-1.1.0"
+  source = "github.com/broadinstitute/terraform-shared.git//terraform-modules/cloudsql-postgres?ref=cloudsql-postgres-2.0.0"
 
   enable = var.enable
 
@@ -14,7 +14,15 @@ module "cloudsql" {
   }
   cloudsql_tier = var.db_tier
 
-  cloudsql_replication_type = null
+  cloudsql_database_flags = {
+    "log_checkpoints" = "on",
+    "log_connections" = "on",
+    "log_disconnections" = "on",
+    "log_lock_waits" = "on",
+    "log_min_error_statement" = "error",
+    "log_temp_files" = "0",
+    "log_min_duration_statement" = "-1"
+  }
 
   app_dbs = {
     "${local.service}" = {
