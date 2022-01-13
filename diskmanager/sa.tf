0c48539abc6e38f18682d81fde9b303a04826daf
Jack Warren <jack@jackwarren.info>
2022-01-13 17:18:52 -0500

    [DDO-1798] Use a custom role for diskmanager (#214)

--- diskmanager/sa.tf
+++ diskmanager/sa.tf
@@ -5,11 +5,25 @@ resource "google_service_account" "diskmanager_sa" {
   display_name = "diskmanager-${local.owner}"
 }
 
+resource "google_project_iam_custom_role" "diskmanager_sa_custom_role" {
+  provider = google.target
+  project     = var.google_project
+  role_id     = "diskmanagerRole"
+  title       = "Diskmanager Custom Role"
+  description = "Custom role for Diskmanager, managed by DevOps Atlantis `diskmanager`"
+  permissions = [
+    # As of 1/13/22 IAM Recommender across all environments
+    # Replaces Compute Storage Admin
+    "compute.disks.list",
+    "compute.resourcePolicies.get"
+  ]
+}
+
 resource "google_project_iam_binding" "diskmanager_sa_role" {
   provider = google.target
   project  = var.google_project
 
-  role = "roles/compute.storageAdmin"
+  role = google_project_iam_custom_role.diskmanager_sa_custom_role.id
   members = [
     "serviceAccount:${google_service_account.diskmanager_sa.email}"
   ]
