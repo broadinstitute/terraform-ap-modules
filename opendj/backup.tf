05d0ca6e2d54749ab1eba3926d8218f672eb006e
Jack Warren <jack@jackwarren.info>
2022-01-18 11:21:11 -0500

    use a custom role

--- opendj/backup.tf
+++ opendj/backup.tf
@@ -46,10 +46,26 @@ resource "google_storage_bucket_iam_binding" "binding" {
   members  = ["serviceAccount:${google_service_account.backup-sa.email}"]
 }
 
+resource "google_project_iam_custom_role" "opendj_backup_sa_custom_role" {
+  provider = google.target
+  project     = var.google_project
+  role_id     = "opendjBackupRole"
+  title       = "OpenDJ Backup Custom Role"
+  description = "Custom role for OpenDJ Backup, managed by DevOps Atlantis `opendj`"
+  permissions = [
+    # As of 1/18/22 IAM Recommender across all environments
+    # Replaces Kubernetes Engine Admin
+    "container.clusters.get",
+    "container.clusters.getCredentials",
+    "container.pods.exec",
+    "container.pods.get"
+  ]
+}
+
 # Allow backup SA to copy backups from pod to bucket
-resource "google_project_iam_member" "backup" {
+resource "google_project_iam_member" "backup_role_membership" {
   provider = google.target
   project  = var.google_project
-  role     = "roles/container.admin"
+  role     = google_project_iam_custom_role.opendj_backup_sa_custom_role.id
   member   = "serviceAccount:${google_service_account.backup-sa.email}"
 }
