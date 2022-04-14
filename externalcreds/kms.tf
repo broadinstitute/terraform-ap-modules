32e11b4a472d8eb3d65917b0f42da9c230154954
yuhuyoyo <yuhuyoyo@google.com>
2022-04-14 14:10:37 -0400

    need to use underscore

--- externalcreds/kms.tf
+++ externalcreds/kms.tf
@@ -4,7 +4,7 @@ resource "google_kms_key_ring" "keyring" {
   project = var.google_project
 }
 
-resource "google_kms_crypto_key" "ssh-keypair-encryption-key" {
+resource "google_kms_crypto_key" "ssh_keypair_encryption_key" {
   name            = var.ssh_keypair_encryption_kms_key
   key_ring        = google_kms_key_ring.keyring.id
 
