5be6af27754f2fbf4b735ab81850b454c84fe871
Yu Hu <93544261+yuhuyoyo@users.noreply.github.com>
2022-04-20 15:07:24 -0400

    add a rotation period of 90 days

--- externalcreds/kms.tf
+++ externalcreds/kms.tf
@@ -7,6 +7,7 @@ resource "google_kms_key_ring" "keyring" {
 resource "google_kms_crypto_key" "ssh_keypair_encryption_key" {
   name            = var.ssh_keypair_encryption_kms_key
   key_ring        = google_kms_key_ring.keyring.id
+  rotation_period = "7776000s"
 
   lifecycle {
     prevent_destroy = true
