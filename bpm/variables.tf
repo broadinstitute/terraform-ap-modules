1c78c3c7a8df585cebf17cb4d645dc9ef1b857ba
Drew Herbst <aherbst@broadinstitute.org>
2022-05-13 17:18:35 -0400

    Add dependencies var

--- bpm/variables.tf
+++ bpm/variables.tf
@@ -1,3 +1,10 @@
+variable "dependencies" {
+  # See: https://github.com/hashicorp/terraform/issues/21418#issuecomment-495818852
+  type        = any
+  default     = []
+  description = "Work-around for Terraform 0.12's lack of support for 'depends_on' in custom modules."
+}
+
 variable "enable" {
   type        = bool
   description = "Enable flag for this module. If set to false, no resources will be created."
