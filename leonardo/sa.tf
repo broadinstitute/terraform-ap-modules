7dddc56888dc8b1bf14516b558da0023a57a467a
Jack Warren <jack@jackwarren.info>
2022-06-03 16:45:51 -0400

    typo

--- leonardo/sa.tf
+++ leonardo/sa.tf
@@ -3,5 +3,5 @@ resource "google_storage_bucket_iam_member" "member" {
   count  = var.enable && contains(keys(var.service_accounts), "leonardo") && length(var.service_accounts["leonardo"]) > 0 ? 1 : 0
   bucket = var.terra_docker_versions_upload_bucket
   role   = "roles/storage.objectAdmin"
-  member = var.service_accounts[leonardo]
+  member = var.service_accounts["leonardo"]
 }
