f0d239be7fc6783aecb53fa8d9858744208ddc8e
Robert Title <rtitle@broadinstitute.org>
2022-09-06 11:43:29 -0400

    Bump WSM ref

--- terra-env/main.tf
+++ terra-env/main.tf
@@ -71,7 +71,7 @@ module "sam" {
   firestore_folder_id          = var.sam_firestore_folder_id
 }
 module "workspace_manager" {
-  source = "github.com/broadinstitute/terraform-ap-modules.git//terra-workspace-manager?ref=TOAZ-199-add-deployment-support-for-lz-databases"
+  source = "github.com/broadinstitute/terraform-ap-modules.git//terra-workspace-manager?ref=terra-workspace-manager-0.10.0"
 
   enable = local.terra_apps["workspace_manager"]
 
