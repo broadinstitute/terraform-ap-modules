fe0297cdd94554a8b55ac74dd7939508a0c4eda2
Jack Warren <jack@jackwarren.info>
2023-04-04 15:09:40 -0400

    fully qualified name, whoops

--- leonardo/workload-identity.tf
+++ leonardo/workload-identity.tf
@@ -7,7 +7,7 @@ data "google_service_account" "leonardo_service_account" {
 resource "google_service_account_iam_member" "leonardo_workload_identity" {
   count              = var.enable && var.enable_workload_identity ? 1 : 0
   provider           = google.target
-  service_account_id = data.google_service_account.leonardo_service_account[0].unique_id
+  service_account_id = data.google_service_account.leonardo_service_account[0].name
   role               = "roles/iam.workloadIdentityUser"
   member             = "serviceAccount:${var.google_project}.svc.id.goog[${local.namespace}/${var.kubernetes_sa_name}]"
 }
