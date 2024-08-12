14cbadb1ccb22a9b9178dd07b360497927e7bbe7
Chelsea Hoover <60902147+choover-broad@users.noreply.github.com>
2024-08-12 11:49:04 -0400

    [DDO-3833] Cleanup migrated modules (#235)

--- datarepo/main.tf
+++
@@ -1,6 +0,0 @@
-/**
- * # Terra Data Repo module
- * 
- * This module creates DNS records for Data Repo in Terra environments.
- *
- */
--- identity-concentrator/provider.tf
+++
@@ -1,7 +0,0 @@
-provider "google" {
-  alias = "target"
-}
-
-provider "google-beta" {
-  alias = "target"
-}
