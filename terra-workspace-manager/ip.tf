085503b4c62de87328325988e23080a158adf6c3
Chelsea Hoover <chelsea@broadinstitute.org>
2024-08-12 11:26:14 -0400

    cleanup migrated modules

--- terra-workspace-manager/ip.tf
+++
@@ -1,7 +0,0 @@
-resource "google_compute_global_address" "ingress_ip" {
-  count = var.enable && var.dns_enabled && contains(["default", "preview"], var.env_type) ? 1 : 0
-
-  provider = google.target
-
-  name = "terra-${var.cluster}-${local.owner}-${local.service}-ip"
-}
